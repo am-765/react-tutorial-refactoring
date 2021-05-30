2fe5889a9c72190fab99c385b3904b6275ee68c1
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-05-30 12:29:09 +0900

    Add calculateWinner

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,27 @@ type SquareProps = {
   onClick: () => void;
 };
 
+const calculateWinner = (squares: SquareValue[]) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i += 1) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+};
+
 const Square: React.VFC<SquareProps> = ({ value, onClick }) => (
   <button className="square" type="button" onClick={onClick}>
     {value}
@@ -21,6 +42,9 @@ const Board = () => {
 
   const handleClick = (i: number) => {
     const currentSquares = squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     currentSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(currentSquares);
     setXIsNext(!xIsNext);
@@ -30,7 +54,13 @@ const Board = () => {
     <Square value={squares[i]} onClick={() => handleClick(i)} />
   );
 
-  const status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
 
   return (
     <div>
