d7cdb70678c45b5c4f767da8332e1855e3f03a5e
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-06-04 00:16:04 +0900

    Use two loop in the part where the squares are lined up on the board

--- src/index.tsx
+++ src/index.tsx
@@ -52,25 +52,17 @@ const Board = (props: BoardProps) => {
     <Square value={props.squares[i]} onClick={() => props.onClick(i)} />
   );
 
-  return (
-    <div>
+  const boardRow = Array(3)
+    .fill(0)
+    .map((_a, i) => (
       <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
+        {Array(3)
+          .fill(0)
+          .map((_b, j) => renderSquare(i * 3 + j))}
       </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-    </div>
-  );
+    ));
+
+  return <div>{boardRow}</div>;
 };
 
 const Game = () => {
